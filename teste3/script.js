efcc07b57b89faf7f6e84cd43c28f3a7ec8fdee4
murilo611 <148803563+murilo611@users.noreply.github.com>
2024-09-21 20:56:30 +0100

    Projeto gato
    Primeiro projeto finalizado

---
+++ teste3/script.js
@@ -0,0 +1,11 @@
+script.js
+
+// Seleciona o botão pelo ID
+const botaoDoacao = document.getElementById('botaoDoacao');
+
+// Adiciona um ouvinte de evento para o clique
+botaoDoacao.addEventListener('click', function() {
+    // Mensagem de agradecimento
+    alert('Muito obrigado pela sua ajuda! Sua doação faz a diferença na vida dos gatinhos.');
+});
+
